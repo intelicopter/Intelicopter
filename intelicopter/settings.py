1e757b26771396d5e940d695dc019ff670a33663
Intelicopter <intelicopter@gmail.com>
2017-06-17 23:06:53 +0800

    Edit

--- intelicopter/settings.py
+++ intelicopter/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'intelicopter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
