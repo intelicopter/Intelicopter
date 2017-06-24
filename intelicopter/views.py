4d65093ea9b9872530653b9ad5cf943ba8aa2bb8
Intelicopter <intelicopter@gmail.com>
2017-06-25 01:15:47 +0800

    Edit

--- intelicopter/views.py
+++ intelicopter/views.py
@@ -61,7 +61,7 @@ def get_relevant_items(request):
 
 
 def create_example_data():
-    qn1 = Question(1, "What is your gender?", question_type=1)
+    qn1 = Question(1, "What is your gender?", 1)
     qn2 = Question(2, "Are you pregnant?", 1)
     qn3 = Question(3, "Are you employed?", 1)
 
