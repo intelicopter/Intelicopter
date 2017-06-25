59ae920258f437cb260afc5a8d7fa8118906fa87
Intelicopter <intelicopter@gmail.com>
2017-06-25 11:21:14 +0800

    Edit

--- intelicopter/models.py
+++ intelicopter/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 
+class Meta:
+    db_table = 'intelicopter'
+
+
 class Question(models.Model):
     id = models.IntegerField(primary_key=True)
     text = models.CharField(max_length=500)
-    question_type = models.IntegerField  # 1 radio, 2 checkbox, 3 textbox, 4 number
+    question_type = models.IntegerField()  # 1 radio, 2 checkbox, 3 textbox, 4 number
 
-    # class Meta:
-    #     db_table = "intelicopter_question"
+    class Meta:
+        db_table = "intelicopter_question"
 
 
 class Option(models.Model):
@@ -15,8 +19,8 @@ class Option(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     option_text = models.CharField(max_length=500)
     
-    # class Meta:
-    #     db_table = "intelicopter_option"
+    class Meta:
+        db_table = "intelicopter_option"
 
 
 class Trigger(models.Model):
@@ -25,5 +29,5 @@ class Trigger(models.Model):
     trigger_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='trigger_question_set')
     trigger_text = models.CharField(max_length=500)
 
-    # class Meta:
-    #     db_table = "intelicopter_trigger"
+    class Meta:
+        db_table = "intelicopter_trigger"
--- intelicopter/views.py
+++ intelicopter/views.py
@@ -61,9 +61,9 @@ def get_relevant_items(request):
 
 
 def create_example_data():
-    qn1 = Question.objects.create(1, "What is your gender?", 1)
-    qn2 = Question.objects.create(2, "Are you pregnant?", 1)
-    qn3 = Question.objects.create(3, "Are you employed?", 1)
+    qn1 = Question.objects.create(id=1, text="What is your gender?", question_type=1)
+    qn2 = Question.objects.create(id=2, text="Are you pregnant?", question_type=1)
+    qn3 = Question.objects.create(id=3, text="Are you employed?", question_type=1)
 
     qn1.save()
     qn2.save()
