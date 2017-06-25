91bd7e06675c8e781b1d6a0d81a26132af476651
Intelicopter <intelicopter@gmail.com>
2017-06-25 11:01:45 +0800

    Edit

--- intelicopter/models.py
+++ intelicopter/models.py
@@ -6,8 +6,8 @@ class Question(models.Model):
     text = models.CharField(max_length=500)
     question_type = models.IntegerField  # 1 radio, 2 checkbox, 3 textbox, 4 number
 
-    class Meta:
-        db_table = "intelicopter_question"
+    # class Meta:
+    #     db_table = "intelicopter_question"
 
 
 class Option(models.Model):
@@ -15,8 +15,8 @@ class Option(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     option_text = models.CharField(max_length=500)
     
-    class Meta:
-        db_table = "intelicopter_option"
+    # class Meta:
+    #     db_table = "intelicopter_option"
 
 
 class Trigger(models.Model):
@@ -25,5 +25,5 @@ class Trigger(models.Model):
     trigger_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='trigger_question_set')
     trigger_text = models.CharField(max_length=500)
 
-    class Meta:
-        db_table = "intelicopter_trigger"
+    # class Meta:
+    #     db_table = "intelicopter_trigger"
