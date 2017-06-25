74f662f0151eab7d544044d402747b1a9a0007c8
Intelicopter <intelicopter@gmail.com>
2017-06-25 11:23:36 +0800

    Edit
    edit

--- intelicopter/models.py
+++ intelicopter/models.py
@@ -1,26 +1,16 @@
 from django.db import models
 
 
-class Meta:
-    db_table = 'intelicopter'
-
-
 class Question(models.Model):
     id = models.IntegerField(primary_key=True)
     text = models.CharField(max_length=500)
     question_type = models.IntegerField()  # 1 radio, 2 checkbox, 3 textbox, 4 number
-
-    class Meta:
-        db_table = "intelicopter_question"
 
 
 class Option(models.Model):
     id = models.IntegerField(primary_key=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     option_text = models.CharField(max_length=500)
-    
-    class Meta:
-        db_table = "intelicopter_option"
 
 
 class Trigger(models.Model):
@@ -28,6 +18,3 @@ class Trigger(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='main_question_set')
     trigger_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='trigger_question_set')
     trigger_text = models.CharField(max_length=500)
-
-    class Meta:
-        db_table = "intelicopter_trigger"
