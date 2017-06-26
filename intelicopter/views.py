b7de1c3529a846fd7d2af9fb5da3a9da11a176fb
Intelicopter <intelicopter@gmail.com>
2017-06-26 11:17:34 +0800

    Edit

--- intelicopter/views.py
+++ intelicopter/views.py
@@ -33,16 +33,16 @@ def process_answer(request):
         data_in_string = request.POST.get['data']  # will be in JSON format shown above
         answers_in_string = request.PORT.get['answers']  # will be in array format
 
-        # process JSON
-        data = json.loads(data_in_string)
-        answers = json.loads(answers_in_string.split())
-
-        highest_question_number = max(data.iterkeys(), key=(lambda key: data[key])) # get the highest key number
-        data[highest_question_number] = answers  # latest qn will be the highest question number previously answered
     except:
         # if first time, initialise data, else, assign answer as value to question key
-        data = {}
-        highest_question_number = 0
+        data = {1:{}}
+
+    # process JSON
+    data = json.loads(data_in_string)
+    answers = json.loads(answers_in_string.split())
+
+    highest_question_number = max(data.iterkeys(), key=(lambda key: data[key]))  # get the highest key number
+    data[highest_question_number] = answers  # latest qn will be the highest question number previously answered
 
     # get latest question object
     next_question_tracker = 1
