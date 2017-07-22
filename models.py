39607d881bc07eaad12c9d6c3c9b5840968c8a19
Intelicopter <intelicopter@gmail.com>
2017-07-22 18:55:12 +0800

    Edit

--- models.py
+++ models.py
@@ -34,12 +34,12 @@ class Group(models.Model):
 
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=2000, default=None)
-    address = models.CharField(max_length=400, default=None)
-    postal_code = models.CharField(max_length=15, default=None)
-    contact_name = models.CharField(max_length=50, default=None)
-    contact_number = models.CharField(max_length=50, default=None)
-    contact_email = models.CharField(max_length=50, default=None)
+    description = models.CharField(max_length=2000, default="")
+    address = models.CharField(max_length=400, default="")
+    postal_code = models.CharField(max_length=15, default="")
+    contact_name = models.CharField(max_length=50, default="")
+    contact_number = models.CharField(max_length=50, default="")
+    contact_email = models.CharField(max_length=50, default="")
 
     class Meta:
         app_label = 'intelicopter'
@@ -49,12 +49,12 @@ class Activity(models.Model):
     id = models.IntegerField(primary_key=True)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=2000, default=None)
-    address = models.CharField(max_length=400, default=None)
-    postal_code = models.CharField(max_length=15, default=None)
-    contact_name = models.CharField(max_length=50, default=None)
-    contact_number = models.CharField(max_length=50, default=None)
-    contact_email = models.CharField(max_length=50, default=None)
+    description = models.CharField(max_length=2000, default="")
+    address = models.CharField(max_length=400, default="")
+    postal_code = models.CharField(max_length=15, default="")
+    contact_name = models.CharField(max_length=50, default="")
+    contact_number = models.CharField(max_length=50, default="")
+    contact_email = models.CharField(max_length=50, default="")
 
     class Meta:
         app_label = 'intelicopter'
@@ -66,8 +66,8 @@ class Criterion(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=500)
     # -2:equals to or less than, -1:less than, 1:more than, 2:equals to or more than
-    range = models.IntegerField(default=None)
-    radio_group_id = models.IntegerField(default=None)
+    range = models.IntegerField(default=None, null=True)
+    radio_group_id = models.IntegerField(default=None, null=True)
 
     class Meta:
         app_label = 'intelicopter'
