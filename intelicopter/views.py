41052b929ef7e34201b732c820bfde610d490003
Intelicopter <intelicopter@gmail.com>
2017-07-22 19:16:35 +0800

    Edit

--- intelicopter/views.py
+++ intelicopter/views.py
@@ -203,20 +203,20 @@ def create_example_data():
     #                                     name="EmployedYesNo Activity")
 
     criterion1 = Criterion.objects.create(id=1,
-                                          activity=Activity.objects.filter(id__exact=1),
-                                          question=Question.objects.filter(id__exact=1),
+                                          activity=Activity.objects.get(id=1),
+                                          question=Question.objects.get(id=1),
                                           question_text="Male")
     criterion2 = Criterion.objects.create(id=2,
-                                          activity=Activity.objects.filter(id__exact=2),
-                                          question=Question.objects.filter(id__exact=2),
+                                          activity=Activity.objects.get(id=2),
+                                          question=Question.objects.get(id=2),
                                           question_text="Yes")
     criterion3 = Criterion.objects.create(id=3,
-                                          activity=Activity.objects.filter(id__exact=4),
-                                          question=Question.objects.filter(id__exact=3),
+                                          activity=Activity.objects.get(id=4),
+                                          question=Question.objects.get(id=3),
                                           question_text="Yes")
     criterion4 = Criterion.objects.create(id=4,
-                                          activity=Activity.objects.filter(id__exact=4),
-                                          question=Question.objects.filter(id__exact=3),
+                                          activity=Activity.objects.get(id=4),
+                                          question=Question.objects.get(id=3),
                                           question_text="No")
 
     #return True
