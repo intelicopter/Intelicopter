4a6c204f5eab6a1c49d426f9ac978561a2a1722a
Intelicopter <intelicopter@gmail.com>
2017-07-24 20:31:54 +0800

    Edit

--- intelicopter/views.py
+++ intelicopter/views.py
@@ -145,7 +145,7 @@ def get_relevant_activities(request, data):
 def check_activity_relevance(data, activity):
     criteria = Criterion.objects.filter(activity=activity)
     number_of_criteria = Criterion.objects.filter(activity=activity).count()
-    pass_counter = 0
+    pass_counter = number_of_criteria
     for criterion in criteria:
         question_number = criterion.question.id
         question_text = criterion.question_text
@@ -154,7 +154,8 @@ def check_activity_relevance(data, activity):
         for answers in data[unicode(str(question_number), "utf-8")]:
             for answer in answers:
                 if answer == "skip":
-                    return False
+                    return pass_counter
+                    #return False
                 elif question_range is None:
                     if question_text == answer:
                         pass_counter += 1
