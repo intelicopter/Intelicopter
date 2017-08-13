5b739e585bdd7650be79112e7459e6a24deb4a9d
Intelicopter <intelicopter@gmail.com>
2017-08-13 09:30:58 +0800

    edit

--- intelicopter/admin.py
+++ intelicopter/admin.py
@@ -4,9 +4,11 @@ from models import Question, Option
 
 class OptionAdminInline(admin.TabularInline):
     model = Option
+    fields = ('option_text')
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    fields = ('text', 'question_type')
     inlines = (OptionAdminInline, )
 
 admin.site.register(Question, QuestionAdmin)
