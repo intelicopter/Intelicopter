be5fba3fe431f37012809ed52635f3f929072591
Intelicopter <intelicopter@gmail.com>
2017-08-13 09:54:47 +0800

    edit

--- intelicopter/admin.py
+++ intelicopter/admin.py
@@ -13,6 +13,12 @@ class OptionAdminInline(admin.TabularInline):
     exclude = ['id']
     formset = MyInline
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
 class QuestionAdmin(admin.ModelAdmin):
     fields = ('text', 'question_type')
     inlines = (OptionAdminInline, )
