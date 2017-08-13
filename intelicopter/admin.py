2b58f95fa7ff172ffb5f41a1618ce930b2a006ca
Intelicopter <intelicopter@gmail.com>
2017-08-13 09:42:04 +0800

    edit

--- intelicopter/admin.py
+++ intelicopter/admin.py
@@ -5,11 +5,12 @@ from models import Question, Option
 class OptionAdminInline(admin.TabularInline):
     model = Option
     exclude = ['id']
-
+    #can_delete = False
+    has_change_permission = False
+    has_delete_permission = False
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ('text', 'question_type')
     inlines = (OptionAdminInline, )
-    can_delete = False
 
 admin.site.register(Question, QuestionAdmin)
