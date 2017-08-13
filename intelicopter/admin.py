c93fbe72e072c3173514091f4088bd75255d9a5f
Intelicopter <intelicopter@gmail.com>
2017-08-13 09:48:14 +0800

    edit

--- intelicopter/admin.py
+++ intelicopter/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
+from django.forms import models
 from models import Question, Option
 
+
+class MyInline(models.BaseInlineFormSet):
+    def __init__(self, *args, **kwargs):
+        super(MyInline, self).__init__(*args, **kwargs)
+        self.can_delete = False
 
 class OptionAdminInline(admin.TabularInline):
     model = Option
     exclude = ['id']
-    #can_delete = False
-    has_change_permission = False
-    has_delete_permission = False
+    formset = MyInline
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ('text', 'question_type')
