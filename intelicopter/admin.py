a9408a1dcc3d01062ac75a89e3a2d01a0ca93270
Intelicopter <intelicopter@gmail.com>
2017-08-13 09:09:05 +0800

    edit

--- intelicopter/admin.py
+++ intelicopter/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-import os
-
 from models import Question, Option
+
 
 class OptionInline(admin.TabularInline):
     model = Option
+
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [
         OptionInline,
     ]
 
-admin.site.register(Question, Option, OptionInline, QuestionAdmin)
+admin.site.register(Question)
+admin.site.register(Option, OptionInline, QuestionAdmin)
