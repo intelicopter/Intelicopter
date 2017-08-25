c57df0016b63caec94a49d86b1994aae4bfac698
Intelicopter <intelicopter@gmail.com>
2017-08-25 20:45:24 +0800

    edit

--- intelicopter/views.py
+++ intelicopter/views.py
@@ -119,7 +119,7 @@ def check_if_triggered(question, data):
         else:
             if trigger_question_id in data:
                 trigger_value = float(trigger.trigger_text)
-                answer = data[trigger_question_id]
+                answer = float(data[trigger_question_id])
                 if trigger.trigger_range is -2 and not answer < trigger_value:
                     return False
                 elif trigger.trigger_range is -1 and not answer <= trigger_value:
